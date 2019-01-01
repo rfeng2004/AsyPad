4cf688bb341468546a7c818e0d26944a4a6c8b1b
Raymond Feng <rfeng2004@gmail.com>
2018-12-31 18:56:51 -0800

    Advance Version Number

--- AsyPad/build/build/src/asypad/ui/AsyPad.java
+++ AsyPad/build/build/src/asypad/ui/AsyPad.java
@@ -35,7 +35,7 @@ public class AsyPad extends Application
 	/**
 	 * Version number of AsyPad.
 	 */
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 
 	/**
 	 * Operating system the AsyPad is running on.
